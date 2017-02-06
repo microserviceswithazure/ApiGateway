26a359849b4358ebd4475101155f3a25af5238ca
Rahul Rai <rahulrai@live.com>
2017-02-06 15:28:01 +1100

    code complete

---
+++ ApiGateway/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿namespace ApiGateway.Controllers
+{
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+
+    using ApiGateway.Models;
+
+    using FireSharp;
+    using FireSharp.Config;
+    using FireSharp.Interfaces;
+
+    public class EmployeeController : ApiController
+    {
+        private readonly IFirebaseClient client;
+
+        public EmployeeController()
+        {
+            IFirebaseConfig config = new FirebaseConfig
+            {
+                AuthSecret = ConfigurationManager.AppSettings["FirebaseSecret"],
+                BasePath = ConfigurationManager.AppSettings["BasePath"]
+            };
+
+            this.client = new FirebaseClient(config);
+        }
+
+        public async Task<HttpResponseMessage> Delete(string alias)
+        {
+            var response = await this.client.DeleteAsync($"employees/{alias}");
+            return new HttpResponseMessage { StatusCode = response.StatusCode };
+        }
+
+        public async Task<Employee> Get(string alias)
+        {
+            var response = await this.client.GetAsync($"employees/{alias}");
+            return response.ResultAs<Employee>();
+        }
+
+        public async Task<IHttpActionResult> Post([FromBody] Employee employee)
+        {
+            var response = await this.client.SetAsync($"employees/{employee.Alias}", employee);
+            return this.Ok(response.ResultAs<Employee>());
+        }
+    }
+}
---
+++ ApiGateway/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiGateway.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Alias { get; set; }
+        public string Designation { get; set; }
+        public int PayLevel { get; set; }
+    }
+}
